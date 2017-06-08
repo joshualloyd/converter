cd493a9a5a7237163d65c5f6b777c336adb15273
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-06-07 22:42:09 -0500

    initial conversion works, but clear second conversion, clear, and color change don't yet

--- converter.js
+++ converter.js
@@ -1,18 +1,29 @@
-function toCelsius () {
-
+function toCelsius (input) {
+	return (input - 32) * (5/9);
 }
 
-function toFahrenheit () {
-
+function toFahrenheit (input) {
+	return (input * (9/5)) + 32;
 }
 
 // Get a reference to the button element in the DOM
 var button = document.getElementById("converter");
+var celsiusRadio = document.getElementById("celsius-radio");
+var farenheitRadio = document.getElementById("farenheit-radio");
+var inputValue = document.getElementById("temperature-input");
+var output = document.getElementById("output");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
   console.log("event", clickEvent);
+  if (celsiusRadio.checked) {
+  	output.innerHTML = toCelsius(parseInt(inputValue.value));
+  } else if (farenheitRadio.checked) {
+  	output.innerHTML = toFahrenheit(parseInt(inputValue.value));
+  } else {
+  	console.log("please check a radio button");
+  }
 }
 
 // Assign a function to be executed when the button is clicked
