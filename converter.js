238e1e7c7b98bb899fc8e2ad9fc9a09fe1cb7b87
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-06-08 15:05:17 -0500

    refactored some more

--- converter.js
+++ converter.js
@@ -1,11 +1,50 @@
+// converts a fahrenheit number to celsius
 function toCelsius (input) {
 	return (input - 32) * (5/9);
 }
 
+// converts a celsius number to fahrenheit
 function toFahrenheit (input) {
 	return (input * (9/5)) + 32;
 }
 
+// Get a reference to the button element in the DOM
+var button = document.getElementById("converter");
+
+// Get a reference to the clear button in the DOM
+var clear = document.getElementById("clear");
+
+// Get a reference to the Celsius radio button in the DOM
+var celsiusRadio = document.getElementById("celsius-radio");
+
+// Get a reference to the Fahrenheit radio button in the DOM
+var fahrenheitRadio = document.getElementById("fahrenheit-radio");
+
+// Get a reference to the Input in the DOM
+var inputValue = document.getElementById("temperature-input");
+
+// Get a reference to the Output in the DOM
+var output = document.getElementById("output");
+
+// Convert button handler that calls the conversion function and text color function
+function converButtonHandler (clickEvent) {
+  console.log("event", clickEvent);
+  convertOutputText();
+  setOutputColor();
+}
+
+// function that takes the input value, converts it, and injects it into the outpul element
+function convertOutputText () {
+  if (celsiusRadio.checked) {
+    output.innerHTML = toCelsius(parseInt(inputValue.value));
+  } else if (fahrenheitRadio.checked) {
+    output.innerHTML = toFahrenheit(parseInt(inputValue.value));
+  } else {
+    console.log("please check a radio button");
+  }
+}
+
+// function that sets the class/color of the output based on the current number
 function setOutputColor () {
   if (( celsiusRadio.checked && Number(output.innerHTML) > 32 )||( fahrenheitRadio.checked && Number(output.innerHTML) > 90 )) {
     output.className = "red";
@@ -16,31 +55,7 @@ function setOutputColor () {
   }
 }
 
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
-var clear = document.getElementById("clear");
-var celsiusRadio = document.getElementById("celsius-radio");
-var fahrenheitRadio = document.getElementById("fahrenheit-radio");
-var inputValue = document.getElementById("temperature-input");
-var output = document.getElementById("output");
-
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
-  if (celsiusRadio.checked) {
-    var tempConvertedToCelsius = toCelsius(parseInt(inputValue.value));
-  	output.innerHTML = tempConvertedToCelsius;
-    setOutputColor();
-  } else if (fahrenheitRadio.checked) {
-    var tempConvertedToFahrenheit = toFahrenheit(parseInt(inputValue.value));
-  	output.innerHTML = tempConvertedToFahrenheit;
-    setOutputColor();
-  } else {
-  	console.log("please check a radio button");
-  }
-}
-
+// clears the input, output and radio buttons
 function clearInput (clearEvent) {
   console.log("event", clearEvent);
   inputValue.value = "";
@@ -49,22 +64,18 @@ function clearInput (clearEvent) {
   fahrenheitRadio.checked = false;
 }
 
+// handler to convert and color the output text after hitting the enter key
 function enterKeyPressHandler (keyPressEvent) {
   console.log("keypress event", keyPressEvent);
-  if (keyPressEvent.key === "Enter" && celsiusRadio.checked) {
-    var tempConvertedToCelsius = toCelsius(parseInt(inputValue.value));
-    output.innerHTML = tempConvertedToCelsius;
-    setOutputColor();
-  } else if (keyPressEvent.key === "Enter" && fahrenheitRadio.checked) {
-    var tempConvertedToFahrenheit = toFahrenheit(parseInt(inputValue.value));
-    output.innerHTML = tempConvertedToFahrenheit;
+  if (keyPressEvent.key === "Enter") {
+    convertOutputText();
     setOutputColor();
   } else {
-    console.log("please check a radio button");
+    console.log("that wasn't the enter key");
   }
 }
 
 // Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+button.addEventListener("click", converButtonHandler);
 window.addEventListener("keydown", enterKeyPressHandler);
 clear.addEventListener("click", clearInput);
